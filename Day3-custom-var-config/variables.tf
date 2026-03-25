cc1bb79638201631c5c6b234933a2384bf4993c0
Jyoti <jyotisoni0112@gmail.com>
2026-03-25 23:14:00 +0530

    Day3

---
+++ Day3-custom-var-config/variables.tf
@@ -0,0 +1,13 @@
+variable "ami_id" {
+    description = "passing value to ami_id"
+    default = ""
+    type = string
+}
+
+variable "instance_type" {
+    description = "passing value to instance_type"
+    default = ""
+    type = string
+
+  
+}
